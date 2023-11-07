0e49c9b5184006f18e97c77fc0285951b77dd42f
RomanTheBaby <mailofrom@gmail.com>
2023-11-06 21:00:15 -0500

    update tournament detail view to show leaderboard if tournament entry is present

--- DrillBuddy/Views/Tournaments/TournamentDetailView.swift
+++ DrillBuddy/Views/Tournaments/TournamentDetailView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct TournamentDetailView: View {
     
-    var tournament: Tournament
+    // MARK: - Properties
+    
+    private var tournament: Tournament
     
     private var gunDescription: String {
         let gunType = tournament.requirements.gunType
@@ -22,76 +25,32 @@ struct TournamentDetailView: View {
         }
     }
     
+    @Query private var tournamentEntries: [TournamentEntry]
+    
+    // MARK: - Init
+    
+    init(tournament: Tournament) {
+        self.tournament = tournament
+        self._tournamentEntries = Query(
+            filter: #Predicate<TournamentEntry> { $0.tournamentId == tournament.id },
+            sort: \TournamentEntry.date,
+            order: .reverse
+        )
+    }
+    
+    // MARK: - View
+    
     var body: some View {
         VStack(spacing: 16) {
             Text(tournament.title)
                 .font(.system(.title, weight: .bold))
                 .frame(maxWidth: .infinity, alignment: .leading)
             
-            VStack(spacing: 8) {
-                HStack {
-                    Text("Start:")
-                        .fontWeight(.medium)
-                    Text(tournament.startDate.formatted(.dateTime.day().month().hour().minute()))
-                    Spacer()
-                }
-                
-                HStack {
-                    Text("End:")
-                        .fontWeight(.medium)
-                    Text(tournament.endDate.formatted(.dateTime.day().month().hour().minute()))
-                    
-                    if Date() > tournament.endDate {
-                        Text("ended")
-                            .foregroundStyle(Color.red)
-                            .font(.callout)
-                    }
-                    Spacer()
-                }
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .foregroundStyle(Color.gray.opacity(0.2))
-            )
-                
-            VStack(alignment: .leading, spacing: 8) {
-                HStack {
-                    Text("Max Shots:")
-                        .fontWeight(.medium)
-                    Text("\(tournament.maxShotsCount == 0 ? "Unlimited" : "\(tournament.maxShotsCount)")")
-                    Spacer()
-                }
-                
-                HStack {
-//                    Image(systemName: "timer")
-                    Text("Time Limit:")
-                        .fontWeight(.medium)
-                    Text("\(tournament.requirements.maxTime == 0 ? "Unlimited" : "\(tournament.requirements.maxTime)")")
-                }
-                
-                HStack {
-                    Text("Gun:")
-                        .fontWeight(.medium)
-                    Text(gunDescription)
-                }
-            }
-            .padding()
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .foregroundStyle(Color.gray.opacity(0.2))
-            )
+            datesView
+            requirementsView
             
             if tournament.description.isEmpty == false {
-                ScrollView {
-                    Text(tournament.description)
-                        .padding()
-                }
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .foregroundStyle(Color.gray.opacity(0.2))
-                )
+                descriptionView
             } else {
                 Spacer()
             }
@@ -100,6 +59,14 @@ struct TournamentDetailView: View {
                 Text("Submissins closed. Tournament has ended")
                     .fontWeight(.bold)
                     .foregroundStyle(Color.gray)
+            } else if tournamentEntries.isEmpty == false {
+                Button(action: {
+                }, label: {
+                    Text("View Leaderboard")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                })
+                .buttonStyle(.borderedProminent)
             } else {
                 Text("Leaderboard will be available after submission")
                     .fontWeight(.heavy)
@@ -127,7 +94,78 @@ struct TournamentDetailView: View {
         }
         .navigationBarTitleDisplayMode(.inline)
         .padding(.horizontal)
-        
+    }
+    
+    // MARK: - Private Views
+    
+    private var datesView: some View {
+        VStack(spacing: 8) {
+            HStack {
+                Text("Start:")
+                    .fontWeight(.medium)
+                Text(tournament.startDate.formatted(.dateTime.day().month().hour().minute()))
+                Spacer()
+            }
+            
+            HStack {
+                Text("End:")
+                    .fontWeight(.medium)
+                Text(tournament.endDate.formatted(.dateTime.day().month().hour().minute()))
+                
+                if Date() > tournament.endDate {
+                    Text("ended")
+                        .foregroundStyle(Color.red)
+                        .font(.callout)
+                }
+                Spacer()
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(Color.gray.opacity(0.2))
+        )
+    }
+    
+    private var requirementsView: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack {
+                Text("Max Shots:")
+                    .fontWeight(.medium)
+                Text("\(tournament.maxShotsCount == 0 ? "Unlimited" : "\(tournament.maxShotsCount)")")
+                Spacer()
+            }
+            
+            HStack {
+//                    Image(systemName: "timer")
+                Text("Time Limit:")
+                    .fontWeight(.medium)
+                Text("\(tournament.requirements.maxTime == 0 ? "Unlimited" : "\(tournament.requirements.maxTime)")")
+            }
+            
+            HStack {
+                Text("Gun:")
+                    .fontWeight(.medium)
+                Text(gunDescription)
+            }
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(Color.gray.opacity(0.2))
+        )
+    }
+    
+    private var descriptionView: some View {
+        ScrollView {
+            Text(tournament.description)
+                .padding()
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(Color.gray.opacity(0.2))
+        )
     }
 }
 
@@ -142,5 +180,12 @@ struct TournamentDetailView: View {
 #Preview("Ended") {
     NavigationStack {
         TournamentDetailView(tournament: TournamentPreviewData.mockEnded)
+    }
+}
+
+#Preview("With Entry") {
+    NavigationStack {
+        TournamentDetailView(tournament: TournamentPreviewData.mock)
+            .modelContainer(TournamentPreviewData.container)
     }
 }
