980d6f97b24bfe5fcf4352e43c1ef708be3e09ee
RomanTheBaby <mailofrom@gmail.com>
2023-12-01 21:50:23 -0500

    fix nan crash

--- DrillBuddy/Resources/Extensions/TimeInterval+Extension.swift
+++ DrillBuddy/Resources/Extensions/TimeInterval+Extension.swift
@@ -10,18 +10,34 @@ import Foundation
 
 extension TimeInterval {
     var hour: Int {
-        Int((self/3600).truncatingRemainder(dividingBy: 3600))
+        guard self.isNaN == false else {
+            return 0
+        }
+        
+        return Int((self / 3600).truncatingRemainder(dividingBy: 3600))
     }
     
     var minute: Int {
-        Int((self/60).truncatingRemainder(dividingBy: 60))
+        guard self.isNaN == false else {
+            return 0
+        }
+        
+        return Int((self / 60).truncatingRemainder(dividingBy: 60))
     }
     
     var second: Int {
-        Int(truncatingRemainder(dividingBy: 60))
+        guard isNaN == false else {
+            return 0
+        }
+        
+        return Int(truncatingRemainder(dividingBy: 60))
     }
     
     var millisecond: Int {
-        Int((self*1000).truncatingRemainder(dividingBy: 1000))
+        guard self.isNaN == false else {
+            return 0
+        }
+        
+        return Int((self * 1000).truncatingRemainder(dividingBy: 1000))
     }
 }
